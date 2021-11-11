08430bbbb30c66f8ef98c9784b8210fbcee15cfb
reidpat <61036489+reidpat@users.noreply.github.com>
2021-11-11 09:53:49 -0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,6 +55,11 @@ export default {
           '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">'
       }
     ],
+    script: [
+      {
+        src: "https://identity.netlify.com/v1/netlify-identity-widget.js"
+      }
+    ],
     __dangerouslyDisableSanitizers: ['noscript']
   },
   /*
@@ -96,7 +101,7 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) { }
   },
   /*
    ** Custom additions configuration
